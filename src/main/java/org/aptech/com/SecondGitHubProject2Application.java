06c43851371778cd7aabb14cc428677b3fc40030
Karlozsanti <onyewekepatrick@gmail.com>
2022-08-09 11:34:21 +0100

    Initial commit

---
+++ src/main/java/org/aptech/com/SecondGitHubProject2Application.java
@@ -0,0 +1,13 @@
+package org.aptech.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondGitHubProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondGitHubProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/aptech/com/SecondGitHubProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aptech.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondGitHubProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
